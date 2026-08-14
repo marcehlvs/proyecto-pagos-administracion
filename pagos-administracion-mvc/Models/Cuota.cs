7cc4a3ac8c52ba14d17a292d6354fe3eae7f0edd
bejarano-matematica <bejarano.jmarcelo@gmail.com>
2026-08-14 13:06:40 -0300

    models

---
+++ pagos-administracion-mvc/Models/Cuota.cs
@@ -0,0 +1,18 @@
+﻿using static pagos_administracion_mvc.Models.Enums;
+
+namespace pagos_administracion_mvc.Models
+{
+    public class Cuota
+    {
+        public int Id { get; set; }
+        public int AlumnoId { get; set; }
+        public Alumno Alumno { get; set; }
+        public int Mes { get; set; }
+        public int Anio { get; set; }
+        public decimal Monto { get; set; } 
+        public DateTime FechaVencimiento { get; set; }
+        public EstadoCuota Estado { get; set; } = EstadoCuota.Pendiente;
+        //Una cuota puede tener varios pagos parciales o reintentos
+        public ICollection<Pago> Pagos { get; set; } = new List<Pago>();
+    }
+}
---
+++ pagos-administracion-mvc/Models/Pago.cs
@@ -0,0 +1,18 @@
+﻿using static pagos_administracion_mvc.Models.Enums;
+
+namespace pagos_administracion_mvc.Models
+{
+    public class Pago
+    {
+        public int Id { get; set; }
+        public int CuotaId { get; set; } 
+        public Cuota Cuota { get; set; }
+        public decimal Monto { get; set; }
+        public DateTime Fecha {  get; set; }
+        public EstadoPago Estado { get; set; }
+
+        public string MercadoPagoPaymentId { get; set; }
+        public string MercadoPagoPreferenceId { get; set; }
+    }
+    
+}
