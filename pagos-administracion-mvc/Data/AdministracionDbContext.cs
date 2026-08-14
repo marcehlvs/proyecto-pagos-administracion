84b0c22eb421db269ba4a0845488c5620559f72c
bejarano-matematica <bejarano.jmarcelo@gmail.com>
2026-08-14 17:26:51 -0300

    AdministracionDbContext

---
+++ pagos-administracion-mvc/Data/AdministracionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pagos_administracion_mvc.Models;
+
+namespace pagos_administracion_mvc.Data
+{
+    public class AdministracionDbContext : DbContext
+    {
+        public AdministracionDbContext(DbContextOptions<AdministracionDbContext> options)
+            : base(options){}
+        public DbSet<Alumno>Alumnos { get; set; }
+        public DbSet<Cuota> Cuotas { get; set; }
+        public DbSet<Pago> Pagos { get; set; }
+        public DbSet<Enums> Enums { get; set; }  
+    }
+}
