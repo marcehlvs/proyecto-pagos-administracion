6abdf45ba754bb851af452a7ac4c2b41fb0e483f
bejarano-matematica <bejarano.jmarcelo@gmail.com>
2026-08-14 13:06:40 -0300

    models

---
+++ pagos-administracion-mvc/Models/Alumno.cs
@@ -0,0 +1,18 @@
+﻿using static pagos_administracion_mvc.Models.Enums;
+
+namespace pagos_administracion_mvc.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Dni { get; set; }
+        public NivelEducativo Nivel { get; set; }
+        public int GradoAnio { get; set; }
+        public Turno Turno { get; set; }
+
+        //Un alumno tiene muchas cuotas (una por mes)
+        public ICollection<Cuota> Cuotas { get; set; } = new List<Cuota>();
+    }
+}
---
+++ pagos-administracion-mvc/Models/Enums.cs
@@ -0,0 +1,30 @@
+﻿namespace pagos_administracion_mvc.Models
+{
+    public class Enums
+    {
+        public enum NivelEducativo
+        {
+            Primaria,
+            Secundaria
+        }
+        public enum Turno
+        {
+            Mañana,
+            Tarde
+        }
+        public enum EstadoCuota
+        {
+            Pendiente,
+            Pagada,
+            Vencida,
+            Parcial
+        }
+        public enum EstadoPago
+        {
+            Pendiente,
+            Aprobado,
+            Rechazado,
+            Cancelado
+        }
+    }
+}
