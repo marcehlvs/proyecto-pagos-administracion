020cf6b84e15493157ee0f7c11483aaf71235c67
bejarano-matematica <bejarano.jmarcelo@gmail.com>
2026-08-15 10:55:10 -0300

    LogicalIdentity

--- pagos-administracion-mvc/Migrations/20260815135322_LogicalIdentity2.Designer.cs
+++ pagos-administracion-mvc/Migrations/20260815135322_LogicalIdentity2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using pagos_administracion_mvc.Data;
 
@@ -11,9 +12,11 @@ using pagos_administracion_mvc.Data;
 namespace pagos_administracion_mvc.Migrations
 {
     [DbContext(typeof(AdministracionDbContext))]
-    partial class AdministracionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260815135322_LogicalIdentity2")]
+    partial class LogicalIdentity2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ pagos-administracion-mvc/Migrations/20260815135322_LogicalIdentity2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pagos_administracion_mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class LogicalIdentity2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
