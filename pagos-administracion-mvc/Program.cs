0f0139cffcbacf7d901d0d5c00a0a75650770ffc
bejarano-matematica <bejarano.jmarcelo@gmail.com>
2026-08-17 23:32:48 -0300

    MercadoPago Webhook EndPoint

--- pagos-administracion-mvc/Program.cs
+++ pagos-administracion-mvc/Program.cs
@@ -1,3 +1,4 @@
+using MercadoPago.Config;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using pagos_administracion_mvc.Data;
@@ -5,55 +6,75 @@ using pagos_administracion_mvc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Registro de Servicios
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
 builder.Services.AddDbContext<AdministracionDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Identity/Account/Login";
     options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);  
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
     options.SlidingExpiration = true;
-}); 
+});
+
 builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
-    {
-        options.SignIn.RequireConfirmedEmail = false;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequireUppercase = false;
-        options.Password.RequireLowercase = false;
-        options.Password.RequiredLength = 6;
-    })
+{
+    options.SignIn.RequireConfirmedEmail = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequiredLength = 6;
+})
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<AdministracionDbContext>();
+
 builder.Services.AddScoped<MercadoPagoService>();
 
 var app = builder.Build();
 
+// Configuración de SDKs y Claves de API
+MercadoPagoConfig.AccessToken = builder.Configuration["MercadoPago:AccessToken"];
+
+// Middlewares de HTTP Pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+
 app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapRazorPages();
 app.MapStaticAssets();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-
+//Seeding / Migraciones de Base de Datos
 using (var scope = app.Services.CreateScope())
 {
-    var context = scope.ServiceProvider.GetRequiredService<AdministracionDbContext>();
-    DbInitializer.Initialize(context);
-    await DbInitializer.SeedRolesAdminAsync(scope.ServiceProvider); 
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AdministracionDbContext>();
+        DbInitializer.Initialize(context);
+        await DbInitializer.SeedRolesAdminAsync(services);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ocurrió un error al ejecutar la siembra de la base de datos.");
+    }
 }
 
 app.Run();
