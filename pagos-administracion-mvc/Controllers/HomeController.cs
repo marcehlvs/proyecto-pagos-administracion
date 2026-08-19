999a9ef75fb493014e01558158610cc6ccb557d7
bejarano-matematica <bejarano.jmarcelo@gmail.com>
2026-08-19 10:27:44 -0300

    Inicio-Nosotros

--- pagos-administracion-mvc/Controllers/HomeController.cs
+++ pagos-administracion-mvc/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pagos_administracion_mvc.Data;
 using pagos_administracion_mvc.Models;
 using System.Diagnostics;
 
@@ -6,15 +8,22 @@ namespace pagos_administracion_mvc.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AdministracionDbContext _context;
+
+        public HomeController(AdministracionDbContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.CantidadAlumnos = await _context.Alumnos.CountAsync();
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Nosotros() => View();
+
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
