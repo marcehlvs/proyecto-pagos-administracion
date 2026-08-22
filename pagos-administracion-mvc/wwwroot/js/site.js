2c2619676b361d666dbf22fb4e11c2159f2cc6b9
bejarano-matematica <bejarano.jmarcelo@gmail.com>
2026-08-22 19:11:50 -0300

    dark mode

--- pagos-administracion-mvc/wwwroot/js/site.js
+++ pagos-administracion-mvc/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var switchEl = document.getElementById('temaSwitch');
+    if (!switchEl) return;
 
-// Write your JavaScript code.
+    switchEl.checked = document.documentElement.getAttribute('data-bs-theme') === 'dark';
+
+    switchEl.addEventListener('change', function () {
+        var nuevoTema = switchEl.checked ? 'dark' : 'light';
+        document.documentElement.setAttribute('data-bs-theme', nuevoTema);
+        localStorage.setItem('tema', nuevoTema);
+    });
+});
