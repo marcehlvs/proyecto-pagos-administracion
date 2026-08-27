66dd9ad43d54e144c4cbca3d1b09517ee68ec893
bejarano-matematica <bejarano.jmarcelo@gmail.com>
2026-08-27 08:37:48 -0300

    migration alumnoActivo

---
+++ pagos-administracion-mvc/Migrations/20260827113452_AlumnoActivo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pagos_administracion_mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlumnoActivo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Activo",
+                table: "Alumnos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Activo",
+                table: "Alumnos");
+        }
+    }
+}
