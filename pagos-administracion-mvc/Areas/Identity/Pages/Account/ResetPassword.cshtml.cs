ab07f9ccf808f3ecacfb9c3e6a454cfd2fa29727
bejarano-matematica <bejarano.jmarcelo@gmail.com>
2026-08-31 13:38:40 -0300

    Translate Pages

--- pagos-administracion-mvc/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
+++ pagos-administracion-mvc/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -40,16 +40,16 @@ public class ResetPasswordModel : PageModel
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "El campo Email es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El Email no es una dirección válida.")]
         public string Email { get; set; } = default!;
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
+        [Required(ErrorMessage = "El campo Contraseña es obligatorio.")]
+        [StringLength(100, ErrorMessage = "La {0} debe tener entre {2} y {1} caracteres.", MinimumLength = 8)]
         [DataType(DataType.Password)]
         public string Password { get; set; } = default!;
 
@@ -58,8 +58,8 @@ public class ResetPasswordModel : PageModel
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "La contraseña y su confirmación no coinciden.")]
         public string? ConfirmPassword { get; set; }
 
         /// <summary>
