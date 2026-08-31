4c0be6dc187769975afe2efacdbada02e2f48b60
bejarano-matematica <bejarano.jmarcelo@gmail.com>
2026-08-31 13:38:40 -0300

    Translate Pages

--- pagos-administracion-mvc/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ pagos-administracion-mvc/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -45,8 +45,8 @@ public class ForgotPasswordModel : PageModel
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "El campo Email es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El Email no es una dirección válida.")]
         public string Email { get; set; } = default!;
     }
 
