89c3a4305527c36cfc9aecb83470e06ae97a190f
bejarano-matematica <bejarano.jmarcelo@gmail.com>
2026-09-07 11:01:31 -0300

    fix Widget

--- pagos-administracion-mvc/Program.cs
+++ pagos-administracion-mvc/Program.cs
@@ -35,7 +35,7 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
 })
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<AdministracionDbContext>();
-
+builder.Services.AddHttpClient();
 builder.Services.AddScoped<MercadoPagoService>();
 builder.Services.AddScoped<AsistenteService>();
 builder.Services.AddHostedService<RevisorVencimientosService>();
