e85d3e215e33c7bcc4b495317ab53e3bd91a0463
bejarano-matematica <bejarano.jmarcelo@gmail.com>
2026-09-14 21:24:40 -0300

    fix boletines adjuntar tarea

---
+++ pagos-administracion-mvc/Migrations/20260915002232_PublicacionBoletinesYAdjuntosTarea.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pagos_administracion_mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class PublicacionBoletinesYAdjuntosTarea : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ArchivoNombreOriginal",
+                table: "Tareas",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ArchivoRuta",
+                table: "Tareas",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArchivoNombreOriginal",
+                table: "Tareas");
+
+            migrationBuilder.DropColumn(
+                name: "ArchivoRuta",
+                table: "Tareas");
+        }
+    }
+}
